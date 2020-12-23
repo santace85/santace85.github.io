4e66bcf6daf3186148609786fb5e3b2d2794aca1
santace85 <santace85@gmail.com>
2020-12-23 15:58:04 -0500

    fix for UTF-8 errors

--- javascript/randomquote.js
+++ javascript/randomquote.js
@@ -5,13 +5,13 @@ var qCounter = 0;
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 
 var quotes = [
-["Life isn’t about getting and having, it’s about giving and being.", "Kevin Kruse"],
+["Life isn't about getting and having, it's about giving and being.", "Kevin Kruse"],
 ["Whatever the mind of man can conceive and believe, it can achieve.", "Napoleon Hill"],
 ["Strive not to be a success, but rather to be of value.", "Albert Einstein"],
-["Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.", "Robert Frost"],
+["Two roads diverged in a wood, and I-I took the one less traveled by, And that has made all the difference.", "Robert Frost"],
 ["I attribute my success to this: I never gave or took any excuse.", "Florence Nightingale"],
-["You miss 100% of the shots you don’t take.", "Wayne Gretzky"],
-["I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.", "Michael Jordan"],
+["You miss 100% of the shots you don't take.", "Wayne Gretzky"],
+["I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.", "Michael Jordan"],
 ["The most difficult thing is the decision to act, the rest is merely tenacity.", "Amelia Earhart"],
 ["Every strike brings me closer to the next home run.", "Babe Ruth"],
 ["Definiteness of purpose is the starting point of all achievement.", "W. Clement Stone"]
