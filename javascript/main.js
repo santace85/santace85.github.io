a16fb6b295426c730642c1e8cd948ae3be6cb6b9
santace85 <santace85@gmail.com>
2020-12-28 13:04:06 -0500

    Other - Prettier edit

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,8 @@
-$( document ).ready(function() {
-	//alert("hello world");
-$(".tributepageiframe").colorbox({iframe:true, innerWidth:425, innerHeight:344});
+$(document).ready(function () {
+  //alert("hello world");
+  $(".tributepageiframe").colorbox({
+    iframe: true,
+    innerWidth: 425,
+    innerHeight: 344,
+  });
 });
